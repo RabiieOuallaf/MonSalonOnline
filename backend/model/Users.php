eccf35703c4aac9307aa9b390ab5e71cdd764f12
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-03-11 22:35:26 +0100

    some improvments on the register api

--- backend/model/Users.php
+++ backend/model/Users.php
@@ -1,4 +1,8 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
     if(file_exists('../core/Database.php')){
         require_once '../core/Database.php';
     }else{
@@ -99,6 +103,7 @@
     // Login Endpoint   
 
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        var_dump($_POST);
         switch ($_POST['type']){
             case 'login':      
                 if(isset($_POST['userrefernce'])){
