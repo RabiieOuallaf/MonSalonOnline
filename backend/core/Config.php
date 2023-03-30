b904b7fbf51975a3f5bb0cfabbef6b95957b472e
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-03-30 09:59:07 +0000

    Fixing problems

--- backend/core/Config.php
+++ backend/core/Config.php
@@ -3,5 +3,5 @@
     define('DB_HOST' , 'localhost');
     define('DB_USER', 'root');
     define('DB_USER_PWD' , '');
-    define('DB_PORT', 3307);
+    define('DB_PORT', 3306);
     define('DB_NAME', 'monsalononline');
